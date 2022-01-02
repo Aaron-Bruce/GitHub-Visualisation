bab5224726f920770de7e2bb8b901ad07153b7bd
Aaron-Bruce <63589893+Aaron-Bruce@users.noreply.github.com>
2022-01-02 22:01:03 +0000

    Listing all the commit ID's for a user

--- src/Lib.hs
+++ src/Lib.hs
@@ -20,8 +20,9 @@ import qualified Servant.Client               as SC
 import           Network.HTTP.Client          (newManager)
 import           Network.HTTP.Client.TLS      (tlsManagerSettings)
 
-import Data.Text hiding (map,intercalate)
-import Data.List (intercalate)
+import Data.Text hiding (map,intercalate, groupBy, concat)
+import Data.List (intercalate, groupBy, sortBy)
+import Data.Either
 
 someFunc :: IO ()
 someFunc = do
@@ -42,12 +43,33 @@ testGitHubCall name =
       (SC.runClientM (GH.getUserRepos (Just "haskell-app") name) =<< env) >>= \case
         Left err -> do
           putStrLn $ "heuston, we have a problem (gettign repos): " ++ show err
-        Right res' -> do
+        Right repos -> do
           putStrLn $ "repositories are:" ++
-            intercalate ", " (map (\(GH.GitHubRepo n _ _ ) -> unpack n) res')
+            intercalate ", " (map (\(GH.GitHubRepo n _ _ ) -> unpack n) repos)
+            -- now lets get the full list of collaborators from repositories
+          partitionEithers <$> mapM (getContribs name) repos >>= \case
+
+            ([], contribs) ->
+              putStrLn $ " contributors are: " ++
+              (intercalate "\n\t" . 
+               map (\(GH.RepoCommit n) -> "[" ++ show n ++ "]") $ concat contribs)
+
+            (ers, _)-> do
+              putStrLn $ "heuston, we have a problem (getting contributors): " ++ show ers
 
 
   where env :: IO SC.ClientEnv
         env = do
           manager <- newManager tlsManagerSettings
           return $ SC.mkClientEnv manager (SC.BaseUrl SC.Http "api.github.com" 80 "")
+        getContribs :: GH.Username -> GH.GitHubRepo -> IO (Either SC.ClientError [GH.RepoCommit])
+        getContribs name (GH.GitHubRepo repo _ _) =
+          SC.runClientM (GH.getRepoCommits (Just "haskell-app") name repo) =<< env
+
+       -- groupContributors :: [GH.RepoContributor] -> [GH.RepoContributor]
+        --groupContributors  = sortBy (\(GH.RepoContributor _ c1) (GH.RepoContributor _ c2) ->  compare c1 c2) .
+        --                     map mapfn .
+        --                     groupBy (\(GH.RepoContributor l1 _) (GH.RepoContributor l2 _) ->  l1 == l2)
+        -- where mapfn :: [GH.RepoContributor] -> GH.RepoContributor
+        --       mapfn xs@((GH.RepoContributor l _):_) = GH.RepoContributor l . sum $
+        --                                               map (\(GH.RepoContributor _ c) -> c)  xs
